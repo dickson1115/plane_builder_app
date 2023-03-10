3c70b9e638086b704d7133b67c27b2cd72835b82
dickson1115 <dicksoncheung15@gmail.com>
2023-03-10 08:37:50 -0800

    SideBar toggle view
    Within the expanded sidebar. there are a buttons group of top, front and side.
    Updated the machines of toggles to different views base on the button the user clicks

--- src/Components/Builder/Builder.js
+++ src/Components/Builder/Builder.js
@@ -3,20 +3,25 @@ import SideBar from '../SideBar/SideBar'
 import BuilderCanvas from './BuilderCanvas/BuilderCanvas'
 import svgElements from '../Variables/svgElements'
 const Builder = () => {
-  const [ interactableItems, setInteractableItems ] = useState([
-    // svgElements['engine_front'],
-    // svgElements['wheel_front'],
-  ])
+  const [interactableItems, setInteractableItems] = useState({
+    front_canvas: [],
+    side_canvas: [],
+    top_canvas: []
+  })
+  
 
   const addInteractableItems = (event) => {
     const target = event.target;
+    const canvasName = target.getAttribute('class').split(' ')[0];
     const svgElement = svgElements[target.getAttribute('svg_name')];
-    setInteractableItems(interactableItems => [...interactableItems, svgElement]);
+    setInteractableItems({...interactableItems, [canvasName]:[...interactableItems[canvasName],svgElement]});
     // setInteractableItems([...interactableItems,svgElement]);
   }
   return (
     <div>
-      <BuilderCanvas interactableItems={interactableItems} />
+      <BuilderCanvas className="front_canvas" interactableItems={interactableItems.front_canvas} />
+      <BuilderCanvas className="side_canvas" interactableItems={interactableItems.side_canvas} />
+      <BuilderCanvas className="top_canvas" interactableItems={interactableItems.top_canvas} />
       <SideBar onClick={addInteractableItems} />
     </div>
 
--- src/Components/SideBar/SideBarExpanded/Pages/PlaneComponents.js
+++ src/Components/SideBar/SideBarExpanded/Pages/PlaneComponents.js
@@ -1,52 +1,126 @@
-import React from 'react'
+import { React, useState } from 'react'
 import styles from './PlaneComponents.module.css'
 import svgElements from '../../../Variables/svgElements';
 import SvgButton from '../../../SvgButton/SvgButton';
 function PlaneComponents(props) {
   const strokeColor = "rgba(0, 0, 0, 0.55)";
+  const [view, setView] = useState("front");
+  const handleChangeView = (event) => {
+    const target = event.target
+    setView(target.textContent.toLowerCase())
+  }
   return (
     <div>
       <div className={"d-flex justify-content-center m-2"}>
         <div className={"btn-group " + styles.btnGroup} role="group" aria-label="Basic example">
-          <button type="button" className={"btn " + styles.btn}>Side</button>
-          <button type="button" className={"btn " + styles.btn}>Top</button>
-          <button type="button" className={"btn " + styles.btn}>Front</button>
+          <button type="button" className={"btn " + styles.btn + " " + ((view==="side") ? styles.active:"")} onClick={handleChangeView}>Side</button>
+          <button type="button" className={"btn " + styles.btn + " " + ((view==="top") ? styles.active:"")} onClick={handleChangeView}>Top</button>
+          <button type="button" className={"btn " + styles.btn + " " + ((view==="front") ? styles.active:"")} onClick={handleChangeView}>Front</button>
         </div>
       </div>
 
-      <div className={styles.container} >
+      {/* front */}
+      <div className={styles.container + " " + ((view==="front") ? styles.active:"")} >
         <div className="row">
-          <SvgButton className={"col " + styles.svgButton} viewBox = "0 0 240 50" width="150" onClick={props.onClick} svg_name="wing_front">
+          <SvgButton className={"front_canvas col " + styles.svgButton} viewBox="0 0 240 50" width="150" onClick={props.onClick} svg_name="wing_front">
             {svgElements.wing_front}
             <div className={styles.title}> Wing </div>
           </SvgButton>
         </div>
 
         <div className="row">
-          <SvgButton className={"col " + styles.svgButton} onClick={props.onClick} svg_name="engine_front">
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="engine_front" >
             {svgElements.engine_front}
             <div className={styles.title}> Engine </div>
           </SvgButton>
-          <SvgButton className={"col " + styles.svgButton} onClick={props.onClick} svg_name="wheel_front">
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="wheel_front">
             {svgElements.wheel_front}
             <div className={styles.title}> Wheel </div>
           </SvgButton>
         </div>
 
         <div className="row h-50" >
-          <SvgButton className={"col " + styles.svgButton} onClick={props.onClick} svg_name="fuselage_front">
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="fuselage_front">
             {svgElements.fuselage_front}
             <div className={styles.title}> Fuselage </div>
           </SvgButton>
 
-          <SvgButton className={"col " + styles.svgButton} height="64.67" onClick={props.onClick} svg_name="strut">
+          <SvgButton className={"front_canvas col " + styles.svgButton} height="64.67" onClick={props.onClick} svg_name="strut">
             {svgElements.strut}
             <div className={styles.title}> Strut </div>
           </SvgButton>
         </div>
-
-
       </div >
+
+      {/* side */}
+      <div className={styles.container  + " " + ((view==="side") ? styles.active:"")} >
+        <div className="row">
+          <SvgButton className={"front_canvas col " + styles.svgButton} viewBox="0 0 240 50" width="150" onClick={props.onClick} svg_name="wing_front">
+            {svgElements.wing_front}
+            <div className={styles.title}> Wing </div>
+          </SvgButton>
+        </div>
+
+        <div className="row">
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="engine_front" >
+            {svgElements.engine_front}
+            <div className={styles.title}> Engine </div>
+          </SvgButton>
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="wheel_front">
+            {svgElements.wheel_front}
+            <div className={styles.title}> Wheel </div>
+          </SvgButton>
+        </div>
+
+        <div className="row h-50" >
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="fuselage_front">
+            {svgElements.fuselage_front}
+            <div className={styles.title}> Fuselage </div>
+          </SvgButton>
+
+          <SvgButton className={"front_canvas col " + styles.svgButton} height="64.67" onClick={props.onClick} svg_name="strut">
+            {svgElements.strut}
+            <div className={styles.title}> Strut </div>
+          </SvgButton>
+        </div>
+      </div >
+
+      {/* top */}
+      {/* <div className={styles.container  + " " + ((view==="top") ? styles.active:"")} >
+        <div className="row">
+          <SvgButton className={"front_canvas col " + styles.svgButton} viewBox="0 0 240 50" width="150" onClick={props.onClick} svg_name="wing_front">
+            {svgElements.wing_front}
+            <div className={styles.title}> Wing </div>
+          </SvgButton>
+        </div>
+
+        <div className="row">
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="engine_front" >
+            {svgElements.engine_front}
+            <div className={styles.title}> Engine </div>
+          </SvgButton>
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="wheel_front">
+            {svgElements.wheel_front}
+            <div className={styles.title}> Wheel </div>
+          </SvgButton>
+        </div>
+
+        <div className="row h-50" >
+          <SvgButton className={"front_canvas col " + styles.svgButton} onClick={props.onClick} svg_name="fuselage_front">
+            {svgElements.fuselage_front}
+            <div className={styles.title}> Fuselage </div>
+          </SvgButton>
+
+          <SvgButton className={"front_canvas col " + styles.svgButton} height="64.67" onClick={props.onClick} svg_name="strut">
+            {svgElements.strut}
+            <div className={styles.title}> Strut </div>
+          </SvgButton>
+        </div>
+      </div > */}
+
+
+
+
     </div>
   )
 }
